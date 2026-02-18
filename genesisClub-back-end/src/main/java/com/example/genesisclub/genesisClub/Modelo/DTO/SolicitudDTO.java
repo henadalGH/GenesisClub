f817e9addc660d4e5a2ce2db3563d2b8b94fab73
henadalGH <hrnnnadal@gmail.com>
2026-02-18 10:38:08 -0300

    Creacion de sitios de socio

--- genesisClub-back-end/src/main/java/com/example/genesisclub/genesisClub/Modelo/DTO/SolicitudDTO.java
+++ genesisClub-back-end/src/main/java/com/example/genesisclub/genesisClub/Modelo/DTO/SolicitudDTO.java
@@ -4,57 +4,19 @@ import java.time.LocalDate;
 
 import com.example.genesisclub.genesisClub.Modelo.Enums.EstadoSolicitudEnums;
 
-public class SolicitudDTO {
+import lombok.Data;
 
+@Data
+public class SolicitudDTO {
+    
     private Long id;
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
     private String nombre;
     private String apellido;
     private String email;
     private String contacto;
     private LocalDate fechaSolicitud;
+    private String password;
     private EstadoSolicitudEnums estado;
-    public String getNombre() {
-        return nombre;
-    }
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    public String getApellido() {
-        return apellido;
-    }
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public String getContacto() {
-        return contacto;
-    }
-    public void setContacto(String contacto) {
-        this.contacto = contacto;
-    }
-    public LocalDate getFechaSolicitud() {
-        return fechaSolicitud;
-    }
-    public void setFechaSolicitud(LocalDate fechaSolicitud) {
-        this.fechaSolicitud = fechaSolicitud;
-    }
-    public EstadoSolicitudEnums getEstado() {
-        return estado;
-    }
-    public void setEstado(EstadoSolicitudEnums estado) {
-        this.estado = estado;
-    }
 
     
 }
