522dfdc284a21a6146717acc937d73b55e6e153e
henadalGH <hrnnnadal@gmail.com>
2026-02-27 16:36:06 -0300

    Coreccion de errores

--- genesisclub-fond-end/src/app/interceptors/auth.interceptor.ts
+++ genesisclub-fond-end/src/app/interceptors/auth.interceptor.ts
@@ -1,32 +1,16 @@
-import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { catchError, throwError } from 'rxjs';
+import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  const router = inject(Router);
+
   const token = localStorage.getItem('token');
 
-  // Clonamos la petición para añadir el token si existe
-  const authReq = token 
-    ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } }) 
-    : req;
+  if (!token) return next(req);
 
-  return next(authReq).pipe(
-    catchError((error: HttpErrorResponse) => {
-      
-      // Manejo de errores de autenticación/autorización
-      if (error.status === 401 || error.status === 403) {
-        
-        // Aquí puedes usar: this.toast.error('Sesión expirada');
-        alert('Tu sesión ha expirado o no tienes permisos suficientes.');
-        
-        localStorage.removeItem('token'); // Limpieza de seguridad
-        router.navigate(['/login']);
-      }
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
 
-      // Devolvemos el error para que el componente también pueda manejarlo si quiere
-      return throwError(() => error);
-    })
-  );
+  return next(authReq);
 };
