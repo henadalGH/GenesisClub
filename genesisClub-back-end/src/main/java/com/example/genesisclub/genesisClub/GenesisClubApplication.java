8c0538c90f38ac29c267b9103c07cea4ab2c2772
henadalGH <hrnnnadal@gmail.com>
2026-03-04 14:24:34 -0300

    subiendo

--- genesisClub-back-end/src/main/java/com/example/genesisclub/genesisClub/GenesisClubApplication.java
+++ genesisClub-back-end/src/main/java/com/example/genesisclub/genesisClub/GenesisClubApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GenesisClubApplication {
 
+	@jakarta.annotation.PostConstruct
+public void init() {
+    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    System.out.println("EL BACKEND ESTA ABIERTO - SI VES ESTO, EL CODIGO SUBIO BIEN");
+    System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
+
 	public static void main(String[] args) {
 		SpringApplication.run(GenesisClubApplication.class, args);
 	}
