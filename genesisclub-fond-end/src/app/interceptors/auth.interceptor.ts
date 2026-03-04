5c3e0202694b197172ad32d62019499d7679e07f
henadalGH <hrnnnadal@gmail.com>
2026-03-04 18:38:00 -0300

    NUEVO

--- genesisclub-fond-end/src/app/interceptors/auth.interceptor.ts
+++ genesisclub-fond-end/src/app/interceptors/auth.interceptor.ts
@@ -1,34 +1,16 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn } from "@angular/common/http";
+
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
-  const url = new URL(req.url);
-  const path = url.pathname;
-
-  const publicPaths = [
-    '/api/auth',
-    '/api/usuario/registro',
-    '/api/solicitud/nuevo',
-    '/email',
-    '/api/invitacion/aceptar'
-  ];
-
-  // 🔥 NO mandar token a públicas
-  if (publicPaths.some(p => path.startsWith(p))) {
-    return next(req);
-  }
-
   const token = localStorage.getItem('token');
 
-  if (!token || token === 'null' || token === 'undefined') {
-    return next(req);
-  }
-
-  return next(
-    req.clone({
+  if (token) {
+    req = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
-    })
-  );
+    });
+  }
+
+  return next(req);
 };
