9d3d268b8eba2e28cae32f4959ad306349438a9d
henadalGH <hrnnnadal@gmail.com>
2026-04-17 06:53:01 -0300

    Mas

--- genesisclub-fond-end/src/app/Jugador/incio-jugador/incio-jugador.ts
+++ genesisclub-fond-end/src/app/Jugador/incio-jugador/incio-jugador.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthServicio } from '../../ServiciosCompartidos/auth-servicio';
 import { HeaderJugador } from "../header-jugador/header-jugador";
+import { RubrosJugador } from "../Rubro/rubros-jugador";
 
 @Component({
   selector: 'app-incio-jugador',
   standalone: true,
-  imports: [RouterModule, HeaderJugador],
+  imports: [RouterModule, HeaderJugador, RubrosJugador],
   templateUrl: './incio-jugador.html',
   styleUrl: './incio-jugador.css',
 })
